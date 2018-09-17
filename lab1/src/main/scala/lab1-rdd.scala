cdf31c27f46f6f809b2d73aae60f25765393b8fc
Andre <andrebrreis@gmail.com>
2018-09-17 21:01:16 +0200

    Finish RDD implementation - using spark native functions

--- lab1/src/main/scala/lab1-rdd.scala
+++ lab1/src/main/scala/lab1-rdd.scala
@@ -20,6 +20,10 @@ object GDeltRDD {
         return "%s-%s-%s".format(date.substring(6,8),date.substring(4,6),date.substring(0,4))
     }
 
+    def myprint(input: (String, Iterable[(String, Int)])) = {
+        printf("%s: ( %s ) \n", input._1, input._2.take(10))
+    }
+
     def main(args: Array[String]) {
         Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
         
@@ -30,16 +34,18 @@ object GDeltRDD {
         .getOrCreate()
         val sc = spark.sparkContext // If you need SparkContext object
 
-        val rawData = sc.textFile("/home/andre/tudelft/supercomputing/lab1/segment/20150218230000.gkg.csv")
+        val rawData = sc.textFile("/home/andre/tudelft/supercomputing/lab1/segment/*.gkg.csv")
         val columns = rawData.map(line => line.split("\t"))
         val fullColumns = columns.filter(list => list.length > 23)
         val noFalsePositives = fullColumns.filter(list => !(list(23) contains "Category"))
         val datedWords = noFalsePositives.map(list => (formatDate(list(1)), list(23).split(";")))
                                          .flatMap(pair => pair._2.map(word => ((pair._1, word.split(",")(0)), 1)))
         val groupedDates = datedWords.reduceByKey((x,y) => x+y)
-        val sorted = groupedDates.sortBy(-_._2)
 
-        sorted.take(10).foreach(println)
+        val sorted = groupedDates.sortBy(-_._2)
+        val sortedByDate = sorted.map(x => (x._1._1, (x._1._2, x._2))).groupByKey()
+                    
+        sortedByDate.collect.foreach(myprint)
 
         spark.stop
   }
